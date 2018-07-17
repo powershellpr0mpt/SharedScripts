3b6888674f95c97bea33d7c8e8e228c2916c0dab
Robert Prüst <robert@powershellpr0mpt.com>
2018-07-18 00:08:07 +0200

    regions and old code cleanup

--- Server/Create-IPAM.ps1
+++ Server/Create-IPAM.ps1
@@ -1,33 +1,32 @@
 ﻿## SCRIPT TO BE RUN ON THE IPAM SERVER
 
-#Install IPAM features
+#region Install IPAM features
 Install-WindowsFeature -Name IPAM -IncludeAllSubFeature -IncludeManagementTools -Verbose
+#endregion
 
-#Install AD PowerShell management tools
+#region Install AD PowerShell management tools
 Install-WindowsFeature -Name RSAT-AD-PowerShell -Verbose
+#endregion
 
-#Create folder to store IPAM database files
+#region Create folder to store IPAM database files
 $Drive = 'E:\'
 $Folder = 'IPAM_Database'
 $IPAM_DBFolder = Join-Path -Path $Drive -ChildPath $Folder
 
 New-Item -Path $Drive -Name $Folder -ItemType Directory
+#endregion
 
-#Check for existing GPOs
-$OldGPOs = Get-GPO -All | Where-Object {$_.DisplayName -like '*OLD_IPAM*'}
-foreach ($OldGPO in $OldGPOs) {
-    Rename-GPO -Name $OldGPO.DisplayName -TargetName "Old_$($OldGPO.DisplayName)"
-}
-
-#Provision IPAM server
+#region Provision IPAM server
 $GpoPrefix = 'IPAM'
 Invoke-IpamServerProvisioning -WidSchemaPath $IPAM_DBFolder -ProvisioningMethod Automatic -GpoPrefix $GpoPrefix -Verbose
+#endregion
 
-#Create IPAM GPOs
+#region Create IPAM GPOs
 $Domain = $env:USERDNSDOMAIN
 Invoke-IpamGpoProvisioning -Domain $Domain -GpoPrefixName $GpoPrefix -DelegatedGpoGroup 'Server Administrators' -PassThru -Force
+#endregion
 
-#Configure GPOs
+#region Configure GPOs
 #Get Computer objects
 $DNSServers = Get-ADComputer -Filter "Name -like 'MS-DNS*'"
 $DCServers = Get-ADComputer -Filter "Name -like 'MS-DC*'"
@@ -52,15 +51,19 @@ foreach ($NPSServer in $NPSServers) {
     Invoke-Command -ComputerName $NPSServer.Name -ScriptBlock {net localgroup "event log readers" "$env:USERDOMAIN\IPAMUG" /add ; gpupdate /force}
 }
 
-#Configure Server Discovery
+#endregion
+
+#region Configure Server Discovery
 Add-IpamDiscoveryDomain -Name $Domain -DiscoverDc $true -DiscoverDns $true -DiscoverDhcp $true -PassThru
+#endregion
 
-
-#Set IPAM Server access to Event Log Readers
+#region Set IPAM Server access to Event Log Readers
 Get-ADGroup "Event Log Readers"| Add-ADGroupMember -Members $env:COMPUTERNAME
+#endregion
 
-#Confirm SMB Shares on DHCP Servers have been correctly created
+#region Confirm SMB Shares on DHCP Servers have been correctly created
 Invoke-Command -ComputerName $DHCPServers.Name {Get-SmbShare -Name DhcpAudit}
+#endregion
 
 #Get IPAM Server SID
 $SID = ((Get-ADComputer $env:COMPUTERNAME).SID).Value
@@ -73,7 +76,7 @@ $SID = ((Get-ADComputer $env:COMPUTERNAME).SID).Value
 #Go to the Security Tab
 #Add -> Select Object Types -> Computer Objects -> Add the IPAM Server -> Select Read Properties only
 
-#Set correct Registry key DNS Servers [re-run if changes have been made to confirm]
+#region Set correct Registry key DNS Servers [re-run if changes have been made to confirm]
 foreach ($DNSServer in $DNSServers) {
     Invoke-Command -ComputerName $DNSServer.Name -ScriptBlock {
         $KeyPath = 'HKLM:\SYSTEM\CurrentControlSet\Services\eventlog\DNS Server'
@@ -89,8 +92,9 @@ foreach ($DNSServer in $DNSServers) {
         }
     }
 }
+#endregion
 
-#Checking DNS Server Service [re-run if changes have been made to confirm]
+#region Checking DNS Server Service [re-run if changes have been made to confirm]
 foreach ($DNSServer in $DNSServers) {
     Invoke-Command -ComputerName $DNSServer.Name -ScriptBlock {
         $CurrentSC = sc.exe sdshow dns
@@ -104,17 +108,21 @@ foreach ($DNSServer in $DNSServers) {
         }
     }
 }
+#endregion
 
-#Start Server Discovery
+#region Start Server Discovery
 Get-ScheduledTask -Taskpath '\Microsoft\Windows\IPAM\' -TaskName 'ServerDiscovery' | Start-ScheduledTask
+#endregion
 
-#Set all discovered servers as Managed
+#region Set all discovered servers as Managed
 Get-IpamServerInventory | Set-IpamServerInventory -ManageabilityStatus Managed -PassThru
+#endregion
 
-#Start Server Discovery
+#region Start Server Discovery
 Get-ScheduledTask -Taskpath '\Microsoft\Windows\IPAM\' -TaskName 'ServerDiscovery' | Start-ScheduledTask
+#endregion
 
-#Start Full management Discovery
+#region Start Full management Discovery
 if ((Get-ScheduledTask -Taskpath '\Microsoft\Windows\IPAM\' -TaskName 'ServerDiscovery').State -eq 'Ready') {
     Write-Host -ForegroundColor Yellow "Server discovery ready, starting full management discovery"
     Get-ScheduledTask -Taskpath '\Microsoft\Windows\IPAM\' | Where-Object {$_.TaskName -ne 'ServerDiscovery'} | Start-ScheduledTask
@@ -122,3 +130,4 @@ if ((Get-ScheduledTask -Taskpath '\Microsoft\Windows\IPAM\' -TaskName 'ServerDis
 else {
     Write-Host -ForegroundColor Red "Server discovery still running, re-run when completed"
 }
+#endregion
